ca89acea4c0d62d7c36f715906a84251bf06e176
Natacha Beck <777590+natacha-beck@users.noreply.github.com>
2026-09-02 18:22:10 -0400

    [FIX] small fixes

--- BrainPortal/config/locales/fr/sed.sh
+++
@@ -1,9 +0,0 @@
-find ./views -type f -name "*.yml" | while read -r file; do
-  sed -e 's/"Bourreaux/"Serveurs de calcul/g' \
-      -e 's/"bourreaux/"serveurs de calcul/g' \
-      -e 's/"Bourreau/"Serveur de calcul/g' \
-      -e 's/"bourreau/"serveur de calcul/g' "$file" > "${file}.tmp"
-  
-  cat "${file}.tmp" > "$file"
-  rm "${file}.tmp"
-done
